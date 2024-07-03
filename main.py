d5ea07973238e344e1c106dfb551a0724054c30c
Rainers Veģis <Rainers.Vegis@VOLBURG.LV>
2024-07-03 11:22:07 +0300

    added logging

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import logging.handlers
 import sys
+import logging
+
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+handler = logging.handlers.TimedRotatingFileHandler("logs/app.log", when="midnight", backupCount=7, delay=True)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel("DEBUG")
+
 
 def greet(name):
+    logger.debug(f"Calling function `greet` with argument `{name}`")
     return f"Hello, {name}"
 
 
@@ -8,4 +19,4 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         print(greet(sys.argv[1]))
     else:
-        print("No arguments were provided")
+        logger.warning("No arguments were provided")
